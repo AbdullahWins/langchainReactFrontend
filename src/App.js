643a5b34ccadaeb589092ceaab1066daa989d96a
Abdullah Al MahMud <abdudevs@gmail.com>
2023-05-21 18:32:44 +0600

    merged 2 api calls into 1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "cal-sans";
 import axios from "axios";
+import { themeChange } from "theme-change";
 
 function App() {
   const [response, setResponse] = useState("");
+  const [error, setError] = useState(null);
 
-  // Function to get response from server.js
-  const handleResponse = () => {
-    // Change the port if you change it in server.js
-    fetch("https://langchainbackend.onrender.com/")
-      .then((res) => res.json())
-      .then((data) => {
-        setResponse(data.text);
-        console.log(data.text);
-      });
-  };
+  useEffect(() => {
+    themeChange(false);
+  }, []);
 
   // Function to send data to server.js when the submit button is clicked
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Submit button clicked");
-    // Send data to server.js
-    axios.post("https://langchainbackend.onrender.com/", {
-      data: e.target[0].value,
-    });
-    e.target[0].value = "";
-    handleResponse();
+    try {
+      const requestData = {
+        data: e.target[0].value,
+      };
+      const axiosResponse = await axios.post(
+        process.env.REACT_APP_LANGCHAIN_BACKEND_BASE_URL,
+        requestData
+      );
+      console.log("Response:", axiosResponse.data);
+      setResponse(axiosResponse.data);
+    } catch (error) {
+      console.error("Error submitting data:", error);
+      setError(
+        "An error occurred while submitting the data. Please try again."
+      );
+    }
   };
 
   // Function to render the response from server.js
@@ -38,21 +41,35 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1 className="title">TITLE GOES HERE</h1>
-
-      <form className="form" onSubmit={handleSubmit}>
-        <input
-          className="input"
-          type="text"
-          placeholder="Enter your prompt here"
-        />
-        <button className="button" type="submit">
-          Submit
-        </button>
-      </form>
-
-      <div className="points">{renderPoints()}</div>
+    <div className="flex flex-col items-center justify-center gap-6 py-12 text-center">
+      <div className="flex flex-col items-center justify-center gap-6 w-full">
+        <select className="gradientselect select select-sm" data-choose-theme>
+          <option disabled value="">
+            Pick a theme
+          </option>
+          <option value="">Default</option>
+          <option value="dark">Dark</option>
+          <option value="light">Light</option>
+          <option value="valentine">Valentine</option>
+          <option value="cyberpunk">Cyberpunk</option>
+        </select>
+        <h1 className="text-3xl font-bold py-6">Company Name Generator AI</h1>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center justify-center gap-6"
+        >
+          <input
+            className="input input-bordered w-72"
+            type="text"
+            placeholder="what the company is about?"
+          />
+          <button className="btn btn-sm normal-case" type="submit">
+            Generate
+          </button>
+        </form>
+        <div className="text-center">{renderPoints()}</div>
+        {error && <div className="text-red-500">{error}</div>}
+      </div>
     </div>
   );
 }
